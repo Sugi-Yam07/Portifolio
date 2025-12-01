426a25b5df08ed37e16d12a6e37955493628b315
Sugi-Yam07 <cienciadedadossugiyamaerick@gmail.com>
2025-12-01 19:38:19 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+// Rolagem suave dos links do menu
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+    link.addEventListener("click", function(e) {
+        e.preventDefault();
+        const alvo = document.querySelector(this.getAttribute("href"));
+        alvo.scrollIntoView({ behavior: "smooth" });
+    });
+});
 
